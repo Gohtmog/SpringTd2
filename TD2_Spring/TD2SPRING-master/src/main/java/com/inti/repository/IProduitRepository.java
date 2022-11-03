2ad4d9641d41125117df512d775e0f9f1a0e8417
jonathan <jonathan@BKE592H>
2022-11-03 11:02:26 +0100

    c

--- TD2_Spring/TD2SPRING-master/src/main/java/com/inti/repository/IProduitRepository.java
+++ TD2_Spring/TD2SPRING-master/src/main/java/com/inti/repository/IProduitRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inti.model.Produit;
 
-public interface IproduitRepository extends JpaRepository<Produit, Integer> {
+public interface IProduitRepository extends JpaRepository<Produit, Integer> {
 
 }
